ac88314383385b945209e21e8c9a1c9ad22257ea
GennadyPromagan <pedro20victor@hotmail.com>
2025-09-24 21:54:34 +0000

    Desafio: nível mestre Criando Movimentos Complexos

--- DesafionívelnovatoMovimentandoasPecasdoxadrez.c
+++ DesafionívelnovatoMovimentandoasPecasdoxadrez.c
@@ -1,60 +1,92 @@
 #include <stdio.h>
 
+
+// RECURIVIDADE DO BISPO
+void recursivoEs(int diagonalBispoEsq) {
+    if (diagonalBispoEsq > 0) {
+        recursivoEs(diagonalBispoEsq - 1);
+        printf("Bispo Diagonal Esquerda\n");
+    }
+}
+    void recursivoDi(int diagonalBispoDir) {
+        if (diagonalBispoDir> 0){
+            recursivoDi(diagonalBispoDir - 1);
+            printf("Bispo Diagonal Direita\n");
+        }
+    }
+
+// RECURVISO TORRE
+
+    void recurvisoEsqTorre(int TorreDir){
+        if( TorreDir > 0){
+            recurvisoEsqTorre(TorreDir - 1);
+            printf("Torre a Direita\n");
+        }
+    }
+
+//RECURSIVO CAVALO ESQUERDA
+
+    void recursivoEsqCavalo(int CavaloEsq){
+        if(CavaloEsq > 0){
+            recursivoEsqCavalo(CavaloEsq - 2);
+            printf("Cima\n");
+            printf("cima\n");
+            printf("Esquerda\n");
+        }
+    }
+
+// RECURSIVO CAVALO DIREITA
+
+    void recursivoDirCavalo(int CavaloDir){
+        if(CavaloDir > 0){
+            recursivoDirCavalo(CavaloDir - 2);
+            printf("Cima\n");
+            printf("cima\n");
+            printf("Direita\n");
+        }
+    }
+
+// RECURSIVO RAINHA
+
+    void recursivoRainha(int RainhaDireita){
+        if(RainhaDireita > 0){
+            recursivoRainha(RainhaDireita - 1);
+            printf("Rainha a Direita\n");
+        }
+    }
+
 int main (){
 
     int rainha = 0;
     int bispo = 0;
     int opcao, opcao2;
     
-
 // Torre movendo a Direita
     printf("------- Torre --------\n");
-    for( int Torre = 0; Torre < 5; Torre++){
-        printf("Torre para a direita\n");
-    }
-
+    recurvisoEsqTorre(5);
 
 // Rainha movendo a esquerda
     printf("---- Rainha ----\n");
-    while (rainha <= 8){
-        
-        printf("Rainha para a esquerda\n");
-        rainha++;
-    }
-
+    recursivoRainha(8);
 // Cavalo
 
     printf("---- Cavalo ----\n");
-    printf("1. Cima, Cima e Direita\n");
-    printf("2. Cima, Cima e Esquerda\n");
+    printf("1. Cima, Cima e Esquerda\n");
+    printf("2. Cima, Cima e Direita\n");
     printf("Escolha o Movimento do Cavalo: ");
     scanf("%d", &opcao2);
 
     switch(opcao2){
         case 1:
-            printf("Duas casas para cima, uma casa para Direita\n");
-            for(int i = 0; i < 2; i++){
-            printf("Cima\n");
-
-            for(int j = 0; j < i; j++){
-            printf("Direita\n");
-        }
+            recursivoEsqCavalo(1);
+        {
+        
         break;
 
         case 2:
-            printf("Duas casas para cima, uma casa para Esquerda\n");
-
-            for(int i = 0; i < 2; i++){
-            printf("Cima\n");
-
-          int j = 0;
-
-            while(j < 1 && i == 1){
-            printf("Esquerda\n");
-
-            j++;
-        }
-    }
+            recursivoDirCavalo(1);
+        {
+    }     
         break;
     default:
         printf("Opcao invalida\n");
@@ -62,7 +94,6 @@ int main (){
     }
 
 }
-
 // Escolher para onde o bispo se move
 
         printf("---- Bispo ----\n");    
@@ -75,7 +106,7 @@ int main (){
         printf("Escolha o movimento do bispo: ");
         scanf("%d", &opcao);
 
-    do{
+    
         switch(opcao){
             case 1:
             printf("Bispo para Cima\n");
@@ -94,20 +125,15 @@ int main (){
             break;
 
             case 5:
-            printf("Bispo par Diagonal Esquerda\n");
+            recursivoEs(5);
             break;
 
             case 6:
-            printf("Bispo para Diagonal Direita\n");
+            recursivoDi(5);
             break;
 
         default:
             printf("Opcao invalida\n");
 
         }
-    bispo++;
-
-    }while(bispo < 5);
-
-    printf("*** Fim ***");
 }
